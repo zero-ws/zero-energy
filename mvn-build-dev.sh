aeff103975e1e49bdb115f369584d200b5707661
silentbalanceyh <silentbalanceyh@126.com>
2024-05-02 16:37:10 +0800

    SMU: Move the interface position

--- mvn-build-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Runtime
-mvn -f source/Zero.Core.Runtime.Metadata clean package install -Dquickly -DskipTests=true -Dmaven.javadoc.skip=true
-# Depend On Runtime.Metadata
-  mvn -f source/Zero.Core.Runtime.Assembly clean package install -Dquickly -DskipTests=true -Dmaven.javadoc.skip=true
-  mvn -f source/Zero.Core.Runtime.Configuration clean package install -Dquickly -DskipTests=true -Dmaven.javadoc.skip=true
-  mvn -f source/Zero.Core.Runtime.Domain clean package install -Dquickly -DskipTests=true -Dmaven.javadoc.skip=true
-  mvn -f source/Zero.Core.Feature.Web.Cache clean package install -Dquickly -DskipTests=true -Dmaven.javadoc.skip=true
-# Compare this snippet from zero-ws/zero-energy/mvn-release.sh:
